33d714909ff29d9407ca22db07d9c904df157948
Joao Faria <joaorafaria@gmail.com>
2023-06-23 15:39:31 +0100

    iteration 2 done

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function ApartmentsList() {
-  return <h1>ApartmentsList</h1>;
+  useEffect(() => {
+    getApartmentsFromApi();
+  }, []);
+
+  const [apartmentsArr, setApartmentsArr] = useState(null);
+
+  const getApartmentsFromApi = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/apartments`)
+      .then((response) => {
+        console.log(response);
+        setApartmentsArr(response.data[0]);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  return apartmentsArr.map((apartmentObj) => {
+    return <div key={apartmentObj.id}></div>;
+  });
 }
 
 export default ApartmentsList;
+
+// useEffect(() => {
+//   axios.get(`${process.env.REACT_APP_API_URL}/characters/${characterId}`)
+//       .then(response => {
+//           setDetails(response.data);
+//       })
+//       .catch(e => console.log(e))
+// }, []);
